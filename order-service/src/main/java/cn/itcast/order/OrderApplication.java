6cb9e41046b3d1d36ea30314ea008993f28e939f
zhangzhengchun <1437237338@qq.com>
2023-10-07 15:44:39 +0800

    修改负载均衡的策略（两种方式）

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,5 +1,7 @@
 package cn.itcast.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,4 +26,9 @@ public class OrderApplication {
     public RestTemplate restTemplate(){
         return  new RestTemplate();
     }
+//
+//    @Bean
+//    public IRule randomRule() {
+//        return new RandomRule();
+//    }
 }
