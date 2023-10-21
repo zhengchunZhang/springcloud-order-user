76f03c7cc976a0cb45fbf8c1d548bba9ed92fd62
zhangzhengchun <1437237338@qq.com>
2023-10-21 11:49:03 +0800

    Feign的最佳实践 方式二（抽取）：将FeignClient抽取为独立模块，并且把接口有关的POJO、默认的Feign配置都放到这个模块中，提供给所有消费者使用

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,7 @@
 package cn.itcast.order.service;
 
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.pojo.User;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +13,16 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private UserClient userClient;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+
+        User user = userClient.findById(order.getUserId());
+        order.setUser(user);
+
         // 4.返回
         return order;
     }
