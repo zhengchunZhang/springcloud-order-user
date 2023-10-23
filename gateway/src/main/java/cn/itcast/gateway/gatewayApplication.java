6b8b169f6e305f4817863d5c19d634216fc12737
zhangzhengchun <1437237338@qq.com>
2023-10-23 23:32:19 +0800

    GateWay网关快速入门

---
+++ gateway/src/main/java/cn/itcast/gateway/gatewayApplication.java
@@ -0,0 +1,11 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class gatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(gatewayApplication.class,args);
+    }
+}
